61417672e8cb3b436fa529cdcb4c7f5c3fccdf59
KevValle <kevinvalle1@gmail.com>
2015-07-27 10:34:09 +0200

    Añadido proyecto

---
+++ test-generator-metamorphic/src/main/java/es/uca/webservices/testgen/metamorphic/reader/BPELExprReaderTest.java
@@ -0,0 +1,29 @@
+package es.uca.webservices.testgen.metamorphic.reader;
+
+import java.io.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class BPELExprReaderTest {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document doc = builder.parse(new File("/home/kevin/Colaboracion/LoanApprovalProcess.bpel"));
+		
+		XPathFactory xPathfactory = XPathFactory.newInstance();
+		XPath xpath = xPathfactory.newXPath();
+		XPathExpression expr = xpath.compile("/");
+		
+		System.out.println(expr.evaluate(doc, XPathConstants.STRING));
+	}
+}
